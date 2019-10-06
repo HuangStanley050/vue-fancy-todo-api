769fa7e1aa44b7f1eac33fbd7c9ceb0d2b4b3c79
Stanley Huang <huang.stanley050@gmail.com>
2019-10-06 16:41:27 +1100

    able to login to firebase and return a token

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import cors from "cors";
 import authRouter from "./routes/auth";
 
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use("/api/auth", authRouter);
--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,33 @@
 import { Request, Response, RequestHandler } from "express";
+import axios from "axios";
+
+const auth_endPoint: string = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${
+  process.env.API_KEY
+}`;
 
 export default {
   login: async (req: Request, res: Response, next: RequestHandler) => {
-    res.send("This is the login route");
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+    let loginResult: { data: { idToken: string; email: string } };
+    const loginData: {
+      email: string;
+      password: string;
+      returnSecureToken: boolean;
+    } = {
+      email,
+      password,
+      returnSecureToken: true
+    };
+    try {
+      loginResult = await axios.post(auth_endPoint, loginData);
+
+      res.json({
+        msg: "login successful",
+        data: { token: loginResult.data.idToken, email: loginResult.data.email }
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
