22059e75fae9ceb95d2b336a99162b0f18f363dd
Stanley Huang <huang.stanley050@gmail.com>
2019-10-10 15:13:17 +1100

    finished Task model

---
+++ src/models/Task.ts
@@ -0,0 +1,33 @@
+import * as mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+export type TaskDocument = mongoose.Document & {
+  title: string;
+  information: string;
+  dudDate: Date;
+  completed: boolean;
+};
+
+const TaskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    information: {
+      type: String,
+      required: true
+    },
+    dueDate: {
+      type: Date,
+      required: false
+    },
+    completed: {
+      type: Boolean,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<TaskDocument>("Task", TaskSchema);
