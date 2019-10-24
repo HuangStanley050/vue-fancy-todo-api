f227553a24e50e6307ef8cb738b0c05487203ad6
Stanley Huang <huang.stanley050@gmail.com>
2019-10-24 21:27:27 +1100

    start working on delete route

--- src/controllers/data.ts
+++ src/controllers/data.ts
@@ -2,12 +2,19 @@ import { Request, Response, NextFunction } from "express";
 import Task from "../models/Task";
 
 export default {
+  deleteTodo: async (req: Request, res: Response, next: NextFunction) => {
+    const taskId = req.params.id;
+    console.log(taskId);
+    res.json({ msg: "delete todo route" });
+  },
   returnTodos: async (req: Request, res: Response, next: NextFunction) => {
     let tasks = await Task.find();
+
     res.json({ msg: "returning todos", tasks });
   },
   createTodo: async (req: Request, res: Response, next: NextFunction) => {
     const { title, information, date, creator } = req.body;
+    let result;
     const task = new Task({
       title,
       information,
@@ -16,10 +23,11 @@ export default {
       completed: false
     });
     try {
-      await task.save();
+      result = await task.save();
+
+      res.json({ msg: "todo created!", newTask: result });
     } catch (err) {
       console.log(err);
     }
-    res.json({ msg: "todo created!" });
   }
 };
--- src/routes/data.ts
+++ src/routes/data.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router
   .post("/createTodo", Middleware.checkAuth, DataController.createTodo)
+  .delete("/deleteTodo/:id", Middleware.checkAuth, DataController.deleteTodo)
   .get("/todos", Middleware.checkAuth, DataController.returnTodos);
 
 export default router;
